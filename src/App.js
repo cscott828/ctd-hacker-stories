95f0316f7fee7e6e93438bad2a1e894b2867fd15
Crystal Scott <85518070+cscott828@users.noreply.github.com>
2021-10-27 16:44:27 -0700

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
 
-function getTitle(title) {
-  return title;
-}
 
-function App() {
-  return (
-    <div>
-      <h1>Hello {getTitle('React')}</h1>
-    </div>
-  );
-}
+
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+  </div>
+);
+
+
 
 export default App;
