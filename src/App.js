634d98c6f2352aebf869329d5df6fa4d685f989a
Crystal Scott <85518070+cscott828@users.noreply.github.com>
2021-10-28 14:14:38 -0700

    props and state intro

--- src/App.js
+++ src/App.js
@@ -36,14 +36,21 @@ const App = () => {
 };
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+
   const handleChange = (event) => {
-    console.log(event);
+    setSearchTerm(event.target.value);
   };
 
   return (
   <div>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" onChange={handleChange} />
+    
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+    </p>
   </div>
   );
 };
