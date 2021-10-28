5c56e46cff8d84fb0ada7898c597782da314790b
Crystal Scott <85518070+cscott828@users.noreply.github.com>
2021-10-28 14:27:54 -0700

    Callback Handler added to pass info up to parent component

--- src/App.js
+++ src/App.js
@@ -22,11 +22,15 @@ const App = () => {
     },
   ];
 
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch}/>
 
       <hr />
 
@@ -35,12 +39,14 @@ const App = () => {
   );
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+
+    props.onSearch(event);
   };
 
   return (
